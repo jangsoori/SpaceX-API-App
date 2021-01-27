975baeeec4f3977d1dcd23f5190f70a4783e8146
jangsoori <46868329+jangsoori@users.noreply.github.com>
2021-01-27 11:26:22 +0000

    Added global font

---
+++ styles/GlobalStyle.js
@@ -0,0 +1,15 @@
+import { Global, css } from "@emotion/react";
+
+import React from "react";
+
+export default function GlobalStyle() {
+  return (
+    <Global
+      styles={css`
+        html {
+          font-family: "Rajdhani", sans-serif;
+        }
+      `}
+    />
+  );
+}
