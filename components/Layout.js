f625a577799f58ba7a7afaabb7738a087b3a59ac
jangsoori <46868329+jangsoori@users.noreply.github.com>
2021-01-27 11:17:57 +0000

    Added background to layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,13 @@
 import React from "react";
 import styled from "@emotion/styled";
 import Navbar from "./Navbar";
+const StyledLayout = styled.div`
+  width: 100%;
+  height: 100vh;
+  background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),
+    url("/background.jpg") no-repeat bottom center / cover;
+`;
 
-const StyledLayout = styled.div``;
 export default function Layout({ children }) {
   return (
     <StyledLayout>
