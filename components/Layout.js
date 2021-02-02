a95470a755d785d32c6c64877b4f66b0d26cf11e
jangsoori <46868329+jangsoori@users.noreply.github.com>
2021-02-02 11:54:50 +0000

    Fixed height on layout

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import styled from "@emotion/styled";
 import Navbar from "./Navbar";
 const StyledLayout = styled.div`
   width: 100%;
-  height: 100%;
+  min-height: 100%;
   background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),
     url("/background.jpg") no-repeat bottom center / cover;
   padding: 1rem;
